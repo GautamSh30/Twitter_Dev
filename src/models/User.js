695bf36ae1f8e2b400bed5e1f9694e7b89ac97c0
Gautam Sharma <gautam2586272@gmail.com>
2024-05-27 12:37:51 +0530

    added like and basic user model

---
+++ src/models/User.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userScehma = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userScehma);
+
+export default User;
---
+++ src/models/like.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema(
+  {
+    onModel: {
+      type: String,
+      required: true,
+      enum: ["Tweet", "Comment"],
+      n,
+    },
+    likeable: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      refPath: "onModel",
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Like = mongoose.model("Like", likeSchema);
+
+export default Like;
