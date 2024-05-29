310df39883cbbacfb17ec2a3a970f84852e1b764
Gautam Sharma <gautam2586272@gmail.com>
2024-05-29 17:39:52 +0530

    updated comment model with refPath

--- src/models/comment.js
+++ src/models/comment.js
@@ -6,8 +6,20 @@ const commentSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    userEmail: {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    onModel: {
       type: String,
+      required: true,
+      enum: ["Tweet", "Comment"],
+    },
+    commentable: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      refPath: "onModel",
     },
   },
   { timestamps: true }
